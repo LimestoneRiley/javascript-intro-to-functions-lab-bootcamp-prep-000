8573f50cdf2a87049bb62744d864e16bc8bbbb15
Robert Riley <rob_a_riley@yahoo.com>
2018-09-11 23:11:53 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -20,8 +20,7 @@ function logWhisper (string)
 
 function sayHiToGrandma (string)
 {
-if (string.toLowerCase() === string)
-  {
+if (string.toLowerCase() === string) {
     return "I can't hear you!";
   }
 }
